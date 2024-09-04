464b0587c6c5f3530e3ac3e64edf072cea2bb953
SangWon7242 <gotkdqja@gmail.com>
2024-09-04 18:13:23 +0900

    각각의 기능을 함수로 분리

--- src/App.js
+++ src/App.js
@@ -5,43 +5,58 @@ function App() {
   const [number, setNumber] = useState(0);
   const [numbersRecord, setNumbersRecord] = useState([]);
 
+  const increaseNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const decreaseNumber = () => {
+    setNumber(number + 1);
+  };
+
   const saveNumbers = () => {
     setNumber(0);
     setNumbersRecord([...numbersRecord, number]);
+  };
+
+  const resetRecord = () => {
+    setNumber(0);
+    setNumbersRecord([]);
+  };
+
+  const numberList = () => {
+    if (numbersRecord.length !== 0) {
+      return (
+        <>
+          <h1>숫자 기록</h1>
+          <nav>
+            <ul>
+              {numbersRecord.map((value, index) => (
+                <li key={index}>
+                  {index + 1}번 : {value}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </>
+      );
+    }
+
+    return <h1>기록 없음</h1>;
   };
 
   return (
     <>
       <div>숫자 : {number}</div>
       <div>
-        <button onClick={() => setNumber(number + 1)}>증가</button>
+        <button onClick={increaseNumber}>증가</button>
         &nbsp;
-        <button onClick={() => setNumber(number - 1)}>감소</button>
+        <button onClick={decreaseNumber}>감소</button>
         &nbsp;
-        <button onClick={() => saveNumbers()}>기록</button>
+        <button onClick={saveNumbers}>기록</button>
         &nbsp;
-        <button
-          onClick={() => {
-            setNumber(0);
-            setNumbersRecord([]);
-          }}
-        >
-          초기화
-        </button>
+        <button onClick={resetRecord}>초기화</button>
       </div>
-      {numbersRecord.length !== 0 ? (
-        <nav>
-          <ul>
-            {numbersRecord.map((value, index) => (
-              <li key={index}>
-                {index + 1}번 : {value}
-              </li>
-            ))}
-          </ul>
-        </nav>
-      ) : (
-        <h1>기록 없음</h1>
-      )}
+      {numberList()}
     </>
   );
 }
