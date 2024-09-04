7e7e54ebced1cb569746fda98cab640ef96b9a70
SangWon7242 <gotkdqja@gmail.com>
2024-09-04 17:38:22 +0900

    숫자 증가, 감소 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <>
-      <div>리액트 시작</div>
+      <div>숫자 : {number}</div>
+      <div>
+        <button onClick={() => setNumber(number + 1)}>증가</button>
+        <button onClick={() => setNumber(number - 1)}>감소</button>
+      </div>
     </>
   );
 }
